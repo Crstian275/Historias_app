8114f58ea8b469f46837c5d1b7755c6a24d89cf5
Crstian275 <ag9800642@gmail.com>
2020-06-30 19:59:32 +0200

    para bajarse la app

--- app/src/main/java/com/example/historias/SplashActivity.java
+++ app/src/main/java/com/example/historias/SplashActivity.java
@@ -2,13 +2,26 @@ package com.example.historias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+    private final int TIEMPO_DE_PANTALLA = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //codigo que va a ejecutarse pasar un tiempo
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },TIEMPO_DE_PANTALLA);
     }
 }
